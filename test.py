c65657a375e636a4b4429b16130c0496a28965a3
angiemerryweather <hi@angie.io>
2014-06-15 22:19:57 +0100

    Finding my way around the Last.fm API a bit more.

--- test.py
+++ test.py
@@ -1,42 +1,54 @@
 __author__ = 'am'
 import os
-
+from collections import namedtuple
 import pylast
 
 
-def load_settings():
-    # read YAML settings file
-    settings_file = "settings.yaml"
-    if os.path.isfile(settings_file):
+def load_credentials():
+    # read YAML settings file into credentials dictionary
+    credentials_file = "settings.yaml"
+    if os.path.isfile(credentials_file):
         import yaml
-        with open(settings_file, "r") as f:
-            settings = yaml.load(f)
+        with open(credentials_file, "r") as f:
+            credentials = yaml.load(f)
     else:
         print "Something is wrong with reading the file."  # very shallow error handling
 
-    return settings
+    return credentials
 
 
-class Credentials:
-    def __init__(self, settings):
-        self.api_key = settings['api_key']
-        self.api_secret = settings['api_secret']
-        self.username = settings['username']
-        self.password = settings['password']
+def network_setup():
+    """
+    Sets up a LastFMNetwork object. This is all that is required for some API calls.get_user.
+    (Most require authentication)
+    """
+    credentials = load_credentials()
+    password_hash = pylast.md5(credentials['password'])
+    network = pylast.LastFMNetwork(api_key=credentials['api_key'], api_secret=credentials['api_secret'],
+                                   username=credentials['username'], password_hash=password_hash)
 
-    def setup(self):
-
-        # create network object for interacting with Last.fm API
-        self.network = pylast.LastFMNetwork(self.api_key, self.api_secret,
-                                            self.username, self.password)
-
-        return self.network
+    return network
 
 
-def get_my_recommended_artists():
-    x = Credentials(load_settings())
-    network = x.setup()
-    user = network.get_user("omgangie")
-    print user
+def get_user():
+    """
+    An authenticated user is required for a lot of the method calls in the Last.fm API
+    """
+    user = network_setup().get_authenticated_user()
+    return user
 
-print get_my_recommended_artists()
+
+def get_my_recommended_artists(user):
+    recommended_artists = user.get_recommended_artists(limit=20)
+    for artist in recommended_artists:
+        print network.get_artist(artist.name)
+
+network = network_setup()
+lastfm_user = get_user()
+"""
+names = [a.name for a in rec_test]
+for name in names:
+    print network.get_artist(name)
+"""
+
+get_my_recommended_artists(get_user())
